11a0e1c1dbc36c07469940881fd7d7c3fc0e8476
montenegrop <pablo.rpm@gmail.com>
2022-06-12 23:19:10 -0300

    event error

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,6 @@
 import "dotenv/config";
-import app from "./app";
+import app from "./app.js";
 const PORT = process.env.PORT;
-app.listen(PORT);
-// , (error) => {
-//     if (!error)
-//       console.log(
-//         "Server is Successfully Running, and App is listening on port " +
-//           process.env.PORT
-//       );
-//     else console.log("Error occured, server can't start", error);
-//   }
+app.listen(PORT).on("error", (error) => {
+    console.log("el error es: ", error);
+});
